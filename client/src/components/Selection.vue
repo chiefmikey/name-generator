6827d52a5378fdb1036f6ec304e975778ef21f77
chiefmikey <wolfemikl@gmail.com>
2021-07-22 13:47:48 -0600

    Migrated Selection component to vue

---
+++ client/src/components/Selection.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="selection">
+    <div>
+      <h5>Birthday:</h5>
+      {{ birthday || '–' }}
+    </div>
+    <div>
+      <h5>Animal:</h5>
+      {{ animal || '–' }}
+    </div>
+    <div>
+      <h5>Fruit:</h5>
+      {{ fruit || '–' }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    birthday: {
+      required: true,
+      type: String,
+    },
+    animal: {
+      required: true,
+      type: String,
+    },
+    fruit: {
+      required: true,
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="stylus"></style>
