52ccef11514ef49309443341b52a106b1ed34e79
chiefmikey <wolfemikl@gmail.com>
2021-07-22 13:41:13 -0600

    Updated token-example syntax

--- token-example.js
+++ token-example.js
@@ -7,23 +7,24 @@ const animalSecret = '';
 let animalTokenType = '';
 let animalToken = '';
 
-const animalAccess = () => new Promise((resolve) => {
-  const access = {
-    method: 'post',
-    url: 'https://api.petfinder.com/v2/oauth2/token',
-    data: `grant_type=client_credentials&client_id=${animalKey}&client_secret=${animalSecret}`,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  };
-  axios(access)
-    .then((getAccess) => {
-      animalTokenType = getAccess.data.token_type;
-      animalToken = getAccess.data.access_token;
-      resolve({ animalTokenType, animalToken });
-    })
-    .catch((error) => console.error('error in access request', error));
-});
+const animalAccess = () =>
+  new Promise((resolve) => {
+    const access = {
+      method: 'post',
+      url: 'https://api.petfinder.com/v2/oauth2/token',
+      data: `grant_type=client_credentials&client_id=${animalKey}&client_secret=${animalSecret}`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+    axios(access)
+      .then((getAccess) => {
+        animalTokenType = getAccess.data.token_type;
+        animalToken = getAccess.data.access_token;
+        resolve({ animalTokenType, animalToken });
+      })
+      .catch((error) => console.error('error in access request', error));
+  });
 
 module.exports = {
   weather,
