dda05bf3c3b26e65fff0522fa7b56332079073c0
chiefmikey <wolfemikl@gmail.com>
2021-07-22 13:46:08 -0600

    Migrated Birthday component to vue

---
+++ client/src/components/Birthday.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <select id="monthSelect" v-model="monthInput">
+        <option value="">Month</option>
+        <option v-for="month in months" :key="month.id" :value="month.date">
+          {{ month.date }}
+        </option>
+      </select>
+      <select id="daySelect" v-model="dayInput">
+        <option value="">Day</option>
+        <option v-for="day in days" :key="day.id" :value="day.date">
+          {{ day.date }}
+        </option>
+      </select>
+      <button class="submit-button" type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  method: {
+    required: true,
+    type: Function,
+  },
+
+  emits: ['logBirthday'],
+
+  data() {
+    return {
+      monthInput: '',
+      dayInput: '',
+      days: [],
+      months: [],
+    };
+  },
+
+  beforeMount() {
+    const monthKey = 0;
+    for (let i = 1; i <= 12; i += 1) {
+      let theMonth = String(i);
+      if (theMonth.length < 2) {
+        theMonth = `0${theMonth}`;
+      }
+      this.months.push({ id: monthKey, date: theMonth });
+      this.monthKey += 1;
+    }
+
+    const dayKey = 0;
+    for (let i = 1; i <= 31; i += 1) {
+      let theDay = String(i);
+      if (theDay.length < 2) {
+        theDay = `0${theDay}`;
+      }
+      this.days.push({ id: dayKey, date: theDay });
+      this.dayKey += 1;
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      const date = `${this.monthInput}-${this.dayInput}`;
+      this.$emit('logBirthday', date);
+      this.monthInput = '';
+      this.dayInput = '';
+    },
+  },
+};
+</script>
+
+<style lang="stylus"></style>
