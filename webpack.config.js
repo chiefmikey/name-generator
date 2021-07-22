8f48199a452616e4a2bf5f5132e5b4bd7ee475ae
chiefmikey <wolfemikl@gmail.com>
2021-07-22 13:37:49 -0600

    Updated webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-const path = require('path');
+import path from 'path';
+import { VueLoaderPlugin } from 'vue-loader';
+
+const __dirname = import.meta.url.slice(7, import.meta.url.lastIndexOf('/'));
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/public');
 
-module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+const vue = 'vue-style-loader';
+const css = 'css-loader';
+
+export default {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.js`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
@@ -12,15 +19,34 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js?/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [vue, css],
+      },
+      {
+        test: /\.scss$/,
+        use: [vue, css, 'sass-loader'],
+      },
+      {
+        test: /\.sass$/,
+        use: [vue, css, 'sass-loader'],
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: [vue, css, 'stylus-loader'],
       },
     ],
   },
+  plugins: [new VueLoaderPlugin()],
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx', '.json', '...'],
   },
 };
