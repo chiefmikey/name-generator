f9009ccddc3662faf616c732255ebb8f9953ea56
chiefmikey <wolfemikl@gmail.com>
2021-07-22 13:38:24 -0600

    Initialized vetur configuration

---
+++ vetur.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  settings: {
+    'vetur.useWorkspaceDependencies': true,
+    'vetur.experimental.templateInterpolationService': true,
+  },
+  projects: [
+    {
+      root: './',
+      package: './package.json',
+      globalComponents: ['./client/src/components/**/*.vue'],
+    },
+  ],
+};
