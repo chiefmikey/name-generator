ef0ea1d7d1e965fa1313ff5681972a48ba19b9b1
chiefmikey <wolfemikl@gmail.com>
2021-07-23 09:38:28 -0600

    Fixed mongoose connection

--- db/index.js
+++ db/index.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const mongoURI = 'mongodb://127.0.0.1:27017/myspace';
 
-const db = mongoose.connect(mongoURI, { useNewUrlParser: true });
+const db = mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
-db
-  .then((res) => console.log(`Connected to: ${mongoURI}`))
-  .catch((error) => {
-    console.error(`There was a problem connecting to mongo at: ${mongoURI}`, error);
-  });
+db.then(() => console.log(`Connected to: ${mongoURI}`)).catch((error) => {
+  console.error(
+    `There was a problem connecting to mongo at: ${mongoURI}`,
+    error,
+  );
+});
 
-module.exports = db;
+export default db;
