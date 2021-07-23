475189360621b580341f45cf28025ada8fc2f587
chiefmikey <wolfemikl@gmail.com>
2021-07-23 09:41:46 -0600

    Updated db post route to koa ctx syntax

--- routes/db/post.js
+++ routes/db/post.js
@@ -1,28 +1,31 @@
+/* eslint-disable no-param-reassign */
 import Router from '@koa/router';
 import Name from '../../db/models/Name.js';
 
 const router = new Router({ prefix: '/post' });
 
-router.post('/', async (req, res) => {
+router.post('/', async (ctx) => {
   try {
     const newName = await new Name({
-      emotion: req.body.emotion,
-      normalEmotion: req.body.normalEmotion,
-      emoEmotion: req.body.emoEmotion,
-      animal: req.body.animal,
-      petName: req.body.petName,
-      birthday: req.body.birthday,
-      fruit: req.body.fruit,
-      sugar: req.body.sugar,
-      result: req.body.result,
-      mainResult: req.body.mainResult,
-      emoResult: req.body.emoResult,
+      emotion: ctx.request.body.emotion,
+      normalEmotion: ctx.request.body.normalEmotion,
+      emoEmotion: ctx.request.body.emoEmotion,
+      animal: ctx.request.body.animal,
+      petName: ctx.request.body.petName,
+      birthday: ctx.request.body.birthday,
+      fruit: ctx.request.body.fruit,
+      sugar: ctx.request.body.sugar,
+      result: ctx.request.body.result,
+      mainResult: ctx.request.body.mainResult,
+      emoResult: ctx.request.body.emoResult,
     });
     await newName.save();
-    res.status(200).send('Saved');
+    ctx.response.status = 200;
+    ctx.response.body = 'Saved';
   } catch (error) {
     console.log(error);
-    res.status(400).send(error);
+    ctx.response.status = 400;
+    ctx.response.body = error;
   }
 });
 
