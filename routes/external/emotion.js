31cf158a0e20cefdac3786f7cfbb00e8661b42e0
chiefmikey <wolfemikl@gmail.com>
2021-07-23 09:42:24 -0600

    Updated external emotion route to koa ctx syntax

--- routes/external/emotion.js
+++ routes/external/emotion.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import Router from '@koa/router';
 import axios from 'axios';
 
@@ -8,18 +9,22 @@ router.get('/', async (ctx) => {
     const emotion = await axios.get(
       'https://api.datamuse.com/words?ml=enthusiastic',
     );
-    ctx.response.status(200).send(emotion.data);
+    ctx.response.status = 200;
+    ctx.body = emotion.data;
   } catch (error) {
-    ctx.response.status(200).send([{ word: 'chief' }]);
+    ctx.response.status = 200;
+    ctx.body = { word: 'chief' };
   }
 });
 
 router.get('/emo', async (ctx) => {
   try {
     const emotion = await axios.get('https://api.datamuse.com/words?ml=sad');
-    ctx.response.status(200).send(emotion.data);
+    ctx.response.status = 200;
+    ctx.body = emotion.data;
   } catch (error) {
-    ctx.response.status(200).send([{ word: 'chief' }]);
+    ctx.response.status = 200;
+    ctx.response.body = { word: 'chief' };
   }
 });
 
