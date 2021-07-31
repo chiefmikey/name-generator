2e30a31fe1ddcdfeb21e70be273e0dfdef8c33a0
chiefmikey <wolfemikl@gmail.com>
2021-07-31 09:16:50 -0600

    Executed cassandra init commands via driver client.execute

--- db/index.js
+++ db/index.js
@@ -1,20 +1,38 @@
 import cassandra from 'cassandra-driver';
 
-const { PlainTextAuthProvider } = cassandra.auth;
+const client = (ks) =>
+  new cassandra.Client({
+    contactPoints: ['ng_db'],
+    localDataCenter: 'datacenter1',
+    credentials: { username: 'cassandra', password: 'cassandra' },
+    keyspace: ks,
+  });
 
-const auth = new PlainTextAuthProvider('cassandra', 'cassandra');
+const init = `CREATE KEYSPACE IF NOT EXISTS user WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };`;
 
-const client = new cassandra.Client({
-  contactPoints: ['0.0.0.0'],
-  localDataCenter: 'datacenter1',
-  keyspace: 'user',
-  authProvider: auth,
-});
+const use = `USE user`;
+
+const construct = `
+  CREATE TABLE IF NOT EXISTS user.name (
+    emotion text,
+    normalEmotion text,
+    emoEmotion text,
+    animal text,
+    petName text,
+    birthday text,
+    fruit text,
+    sugar text,
+    mainResult text,
+    emoResult text,
+    PRIMARY KEY (birthday, fruit, animal)
+  );
+`;
+
+client('system').execute(init);
+
+setTimeout(() => {
+  client('system').execute(use);
+  client('user').execute(construct);
+}, 20000);
 
 export default client;
-
-// const query = 'SELECT name, email FROM users WHERE key = ?';
-
-// client
-//   .execute(query, ['someone'])
-//   .then((result) => console.log('User with email %s', result.rows[0].email));
