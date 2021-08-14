ef4ab9421a450450ed28a2ffd864b6eb686877e7
chiefmikey <wolfemikl@gmail.com>
2021-08-13 23:25:19 -0600

    Update entrypoint script

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,8 @@
-FROM node:latest
+FROM alpine:latest
 EXPOSE 3001
 WORKDIR /name-generator
 COPY . .
 COPY init.sh /bin
 RUN rm /name-generator/init.sh
-RUN chown -R node:node /name-generator
-RUN chown node:node /bin/init.sh
 RUN chmod +x /bin/init.sh
-USER node
 ENTRYPOINT "init.sh"
--- init.sh
+++ init.sh
@@ -4,8 +4,16 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
 touch $CONTAINER_ALREADY_STARTED
   echo "-- First container startup --"
-  npm ci && npm start
+  apk update && apk upgrade
+  apk add --no-cache \
+    bash \
+    sudo \
+    nodejs \
+    npm
+  addgroup -S node && adduser -S -D -G node node
+  chown -R :node . && chmod -R 774 .
+  su -s /bin/bash -c 'npm ci && npm start' node
 else
   echo "-- Not first container startup --"
-  npm start
+  su -s /bin/bash -c 'npm ci && npm start' node
 fi
