9f813c9fbe4863843e8b819517ea48afc0e7aa06
Mikl Wolfe <wolfemikl@gmail.com>
2026-09-11 16:26:12 -0600

    feat: add PostHog analytics

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { capturePageView, initAnalytics } from '@mikl/analytics/vanilla';
 import { createApp } from 'vue';
 
 import App from './components/App.vue';
+
+initAnalytics();
+capturePageView();
 
 createApp(App).mount('#app');
